487c0d703c3f30dc4770858887c701a60a9c1a30
David Fowler <davidfowl@gmail.com>
2019-11-15 22:03:37 -0800

    Don't rely on this

--- signalr-node-server/app.js
+++ signalr-node-server/app.js
@@ -14,13 +14,13 @@ chat.on('disconnect', (id) => {
     console.log(`${id} disconnected`);
 });
 
-chat.on('send', function (message) {
-    this.clients.all.send('send', message);
+chat.on('send', (message) => {
+    chat.clients.all.send('send', message);
 });
 
 app.use(express.static('public'));
 
-http.listen(port, function(){
+http.listen(port, () => {
     console.log(`listening on *:${port}`);
 });
 
