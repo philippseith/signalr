461d39294219f1bcce39b3e66c358840b51320d4
David Fowler <davidfowl@gmail.com>
2019-11-25 23:40:38 -0800

    Less files

--- signalr-go-server/cmd/main.go
+++ signalr-go-server/cmd/main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
-	"../pkg/hubs"
 	"../pkg/signalr"
 )
+
+type chat struct {
+	signalr.Hub
+}
+
+func (c *chat) OnConnected(connectionID string) {
+	fmt.Printf("%s connected\n", connectionID)
+
+	c.Groups().AddToGroup("group", connectionID)
+}
+
+func (c *chat) OnDisconnected(connectionID string) {
+	fmt.Printf("%s disconnected\n", connectionID)
+
+	c.Groups().RemoveFromGroup("group", connectionID)
+}
+
+func (c *chat) Send(message string) {
+	c.Clients().Group("group").Send("send", message)
+}
 
 func main() {
 	router := http.NewServeMux()
 	router.Handle("/", http.FileServer(http.Dir("../public")))
 
-	signalr.MapHub(router, "/chat", hubs.NewChat())
+	signalr.MapHub(router, "/chat", &chat{})
 
 	if err := http.ListenAndServe("localhost:8086", router); err != nil {
 		log.Fatal("ListenAndServe:", err)
