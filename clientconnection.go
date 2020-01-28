c09944ad03529b7319c486c40d47b1f567ab5c73
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2020-01-28 22:22:38 +0100

    new: Client interface

--- clientconnection.go
+++ clientconnection.go
@@ -2,6 +2,7 @@ package signalr
 
 type ClientConnection interface {
 	Start() <-chan error
+	Close() <-chan error
 	Closed() <-chan error
 	Invoke(method string, arguments ...interface{}) (<-chan interface{}, <-chan error)
 	Stream(method string, arguments ...interface{}) (<-chan interface{}, <-chan error)
