296e53cb33da4a1d212ebce01ab96d335f57bad0
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2020-10-30 07:45:30 +0100

    chg: Fix test

--- websocketserver_test.go
+++ websocketserver_test.go
@@ -203,7 +203,7 @@ func handShakeAndCallWebSocketTestServer(port int, connectionID string) {
 	}()
 	wsConn := newWebSocketConnection(connectionID, ws)
 	wsConn.initCancel(context.TODO())
-	cliConn := newHubConnection(context.TODO(), wsConn, &protocol, 1<<15)
+	cliConn := newHubConnection(context.TODO(), wsConn, &protocol, 1<<15, log.NewLogfmtLogger(os.Stderr))
 	_, _ = wsConn.Write(append([]byte(`{"protocol": "json","version": 1}`), 30))
 	_, _ = wsConn.Write(append([]byte(`{"type":1,"invocationId":"666","target":"add2","arguments":[1]}`), 30))
 	cliConn.Start()
