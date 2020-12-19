36fb5f07fd64bdf52af33fc76ae363577ea42676
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2020-12-19 10:51:41 +0100

    chg: Extended documentation

--- doc.go
+++ doc.go
@@ -1,22 +1,22 @@
 /*
-package signalr contains a signalr client and a signalr server.
-Both support the signalr transport types Websockets and Server-Sent Events and the transfer format Text (JSON).
+package signalr contains a SignalR client and a SignalR server.
+Both support the transport types Websockets and Server-Sent Events and the transfer format Text (JSON).
 Rudimentary support for binary transfer format (MessagePack) is available.
-For a deeper understanding of signalr see https://github.com/dotnet/aspnetcore/blob/master/src/SignalR/docs/specs/HubProtocol.md
-and https://github.com/dotnet/aspnetcore/blob/master/src/SignalR/docs/specs/TransportProtocols.md
 
 Basics
 
 The SignalR Protocol is a protocol for two-way RPC over any Message-based transport.
 Either party in the connection may invoke procedures on the other party,
 and procedures can return zero or more results or an error.
+Typically, SignalR connections are HTTP-based, but it is dead simple to implement a signalr.Connection on any transport
+that supports io.Reader and io.Writer.
 
 Client
 
-A Client can be used in client side code to access server methods. It s created with NewClient(), which gets the connection
-to the server. A special case is NewHTTPClient(), which only gets the server address and negotiates with the server
+A Client can be used in client side code to access server methods. From an existing connection, it is created with NewClient().
+A special case is NewHTTPClient(), which creates a Client from a server address and negotiates with the server
 which kind of connection (Websockets, Server-Sent Events) will be used.
-The object which will server callbacks is passed to NewClient() / NewHTTPClient() with the Receiver option.
+The object which will receive server callbacks is passed to NewClient() / NewHTTPClient() by using the Receiver option.
 After calling client.Start(), the client is ready to call server methods or to receive callbacks.
 
 Server
@@ -29,15 +29,17 @@ servers context is canceled.
 To server a HTTP connection, use server.MapHTTP(), which connects the server with a path in an http.ServeMux.
 The server then automatically negotiates, which kind of connection (Websockets, Server-Sent Events) will be used.
 
-Supported Hub / Receiver method parameter and return types
+Supported method signatures
 
+The SignalR protocol constrains the signature of hub or receiver methods that can be used over SignalR.
 All methods with serializable types as parameters and return types are supported.
-Methods with multiple return values are supported.
-Hub methods with a single sending channel (<-chan) are used to initiate server side streaming.
-The client will receive the contents of the channel as stream.
+Methods with multiple serializable return values are supported.
+Methods which return single sending channel (<-chan) are used to initiate callee side streaming.
+The caller will receive the contents of the channel as stream.
 When the returned channel is closed, the stream will be completed.
-Hub methods with one or multiple receiving channels (chan<-) as parameters are used as receivers for client side streaming.
-The client invokes this method and pushes one or multiple streams to the server. The method should end when all channels
+Methods with one or multiple receiving channels (chan<-) as parameters are used as receivers for caller side streaming.
+The caller invokes this method and pushes one or multiple streams to the callee. The method should end when all channels
 are closed. A channel is closed by the server when the assigned stream is completed.
+In most cases, the caller will be the client and the callee the server. But the vice versa case is also possible.
 */
 package signalr
