639fa8f69a2a33349118fa10d03e0cee46f6c59c
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2021-01-17 14:05:24 +0100

    chg: e2e test for messagepack

--- signalr_test/spec/server.spec.ts
+++ signalr_test/spec/server.spec.ts
@@ -1,6 +1,7 @@
 import * as signalR from '@microsoft/signalr';
 import { Subject, from } from 'rxjs'
 import { eachValueFrom } from 'rxjs-for-await';
+import {MessagePackHubProtocol} from "@microsoft/signalr-protocol-msgpack";
 
 // IMPORTANT: When a proxy (e.g. px) is in use, the server will get the request,
 // but the client will not get the response
@@ -18,21 +19,37 @@ describe("smoke test", () => {
                 .build();
             await connection.start();
             const pong = await connection.invoke("ping");
-            expect(pong).toEqual("Pong");
+            expect(pong).toEqual("Pong!");
             await connection.stop();
         });
 });
+
+describe("MessagePack smoke test", () => {
+    it("should connect on a clients request for connection and answer a simple request",
+        async () => {
+            const connection: signalR.HubConnection = builder
+                .withUrl("http://127.0.0.1:5001/hub")
+                .withHubProtocol(new MessagePackHubProtocol())
+                .build();
+            await connection.start();
+            const pong = await connection.invoke("ping");
+            expect(pong).toEqual("Pong!");
+            await connection.stop();
+        });
+});
+
 
 class AlcoholicContent {
     drink: string
     strength: number
 }
 
-describe("e2e test with microsoft/signalr client", () =>{
+function runE2E(protocol: signalR.IHubProtocol) {
     let connection: signalR.HubConnection;
     beforeEach(async() => {
         connection = builder
             .withUrl("http://127.0.0.1:5001/hub")
+            .withHubProtocol(protocol)
             .build();
         await connection.start();
     })
@@ -41,22 +58,22 @@ describe("e2e test with microsoft/signalr client", () =>{
     })
     it("should answer a simple request", async () => {
         const pong = await connection.invoke("ping");
-        expect(pong).toEqual("Pong");
+        expect(pong).toEqual("Pong!");
     })
     it("should answer a simple request with multiple results", async () => {
         const triple = await connection.invoke("triumphantTriple", "1.FC Bayern München");
         expect(triple).toEqual(["German Championship", "DFB Cup", "Champions League"]);
     })
     it("should answer a request with an resulting array of structs", async () => {
-        const contents: AlcoholicContent[] = await connection.invoke("alcoholicContents");
+        const contents = await connection.invoke("alcoholicContents");
         expect(contents.length).toEqual(3);
         expect(contents[0].drink).toEqual('Brunello');
-        expect(contents[2].strength).toEqual(56.2);
+        expect(Math.abs(contents[2].strength- 56.2)).toBeLessThan(0.0001);
     })
     it("should answer a request with an resulting map", async () => {
         const contents = await connection.invoke("alcoholicContentMap");
         expect(contents["Beer"]).toEqual(4.9);
-        expect(contents["Lagavulin Cask Strength"]).toEqual(56.2);
+        expect(Math.abs(contents["Lagavulin Cask Strength"] - 56.2)).toBeLessThan(0.0001);
     })
 
     it("should receive a stream", async () => {
@@ -97,4 +114,12 @@ describe("e2e test with microsoft/signalr client", () =>{
         connection.send("touch");
         await p;
     })
-});
+}
+
+describe("e2e test with microsoft/signalr client", () => {
+    runE2E(new signalR.JsonHubProtocol());
+})
+
+describe("MessagePack e2e test with microsoft/signalr client", () => {
+    runE2E(new MessagePackHubProtocol());
+})
