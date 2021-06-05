5be430a5517b861f6fb98c642faf94f18324c1e5
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2021-06-05 11:26:49 +0200

    new: Server.HubClients getter test

---
+++ server_test.go
@@ -0,0 +1,52 @@
+package signalr
+
+import (
+	"context"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"time"
+)
+
+var _ = Describe("Server.HubClients", func() {
+	Context("All().Send()", func() {
+		It("should send clients", func(done Done) {
+			// Create a simple server
+			server, err := NewServer(context.TODO(), SimpleHubFactory(&simpleHub{}),
+				testLoggerOption(),
+				ChanReceiveTimeout(200*time.Millisecond),
+				StreamBufferCapacity(5))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(server).NotTo(BeNil())
+			// Create both ends of the connection
+			cliConn, srvConn := newClientServerConnections()
+			// Start the server
+			go server.Serve(srvConn)
+			// Create the Client
+			receiver := &simpleReceiver{}
+			clientConn, _ := NewClient(context.TODO(), cliConn,
+				Receiver(receiver),
+				testLoggerOption(),
+				TransferFormat("Text"))
+			Expect(clientConn).NotTo(BeNil())
+			// Start it
+			_ = clientConn.Start()
+			// Send from the server to "all" clients
+			server.HubClients().All().Send("OnCallback", "All")
+			ch := make(chan string, 1)
+			go func() {
+				for {
+					if result, ok := receiver.result.Load().(string); ok {
+						ch <- result
+						close(ch)
+						break
+					}
+				}
+			}()
+			// Did the receiver get what we did send?
+			Expect(<-ch).To(Equal("All"))
+			_ = clientConn.Stop()
+			server.cancel()
+			close(done)
+		}, 1.0)
+	})
+})
