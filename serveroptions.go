215b3005c99054b45e4672358e8a9bf08da2e3c0
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2021-09-19 14:42:02 +0200

    chg: lint documentation

--- serveroptions.go
+++ serveroptions.go
@@ -6,7 +6,7 @@ import (
 	"reflect"
 )
 
-// The Server option UseHub sets the hub instance used by the server
+// UseHub sets the hub instance used by the server
 func UseHub(hub HubInterface) func(Party) error {
 	return func(p Party) error {
 		if s, ok := p.(*server); ok {
@@ -17,10 +17,10 @@ func UseHub(hub HubInterface) func(Party) error {
 	}
 }
 
-// The Server option HubFactory sets the function which returns the hub instance for every hub method invocation
+// HubFactory sets the function which returns the hub instance for every hub method invocation
 // The function might create a new hub instance on every invocation.
 // If hub instances should be created and initialized by a DI framework,
-// the frameworks factory method can be called here.
+// the frameworks' factory method can be called here.
 func HubFactory(factoryFunc func() HubInterface) func(Party) error {
 	return func(p Party) error {
 		if s, ok := p.(*server); ok {
@@ -31,7 +31,7 @@ func HubFactory(factoryFunc func() HubInterface) func(Party) error {
 	}
 }
 
-// The Server option SimpleHubFactory sets a HubFactory which creates a new hub with the underlying type
+// SimpleHubFactory sets a HubFactory which creates a new hub with the underlying type
 // of hubProto on each hub method invocation.
 func SimpleHubFactory(hubProto HubInterface) func(Party) error {
 	return HubFactory(
@@ -40,7 +40,7 @@ func SimpleHubFactory(hubProto HubInterface) func(Party) error {
 		})
 }
 
-// The Server option HTTPTransports sets the list of available transports for http connections. Allowed transports are
+// HTTPTransports sets the list of available transports for http connections. Allowed transports are
 // "WebSockets", "ServerSentEvents". Default is both transports are available.
 func HTTPTransports(transports ...string) func(Party) error {
 	return func(p Party) error {
