f0438995c61374970de19dc58407e417f181cb05
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2021-10-30 13:40:38 +0200

    chg: router package doc

---
+++ router/doc.go
@@ -0,0 +1,7 @@
+/*
+Package router contains signalr.MappableRouter factories for some popular http routers.
+The factories can be used to integrate the signalR server from github.com/philippseith/signalr with the router.
+If you don't like to reference router modules you aren't using, just copy the source code
+for the factory for your router.
+*/
+package router
