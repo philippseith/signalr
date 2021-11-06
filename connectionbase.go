9751ec7f8bb2aa8276a47d4ec4f76d19069b4adf
Philipp Seith <6890503+philippseith@users.noreply.github.com>
2021-11-06 17:09:10 +0100

    chg: extend ConnectionBase
    ConnectionBase now contains methods for
    handling parallel Read and Write operations
    with one single timeout

--- connectionbase.go
+++ connectionbase.go
@@ -2,37 +2,67 @@ package signalr
 
 import (
 	"context"
+	"sync"
 	"time"
 )
 
 // ConnectionBase is a baseclass for implementers of the Connection interface.
 type ConnectionBase struct {
-	ctx          context.Context
-	connectionID string
-	timeout      time.Duration
+	mx            sync.RWMutex
+	ctx           context.Context
+	connectionID  string
+	timeout       time.Duration
+	watchDogQueue connectionWatchDogQueue
+}
+
+func NewConnectionBase(ctx context.Context, connectionID string) ConnectionBase {
+	cb := ConnectionBase{
+		ctx:           ctx,
+		connectionID:  connectionID,
+		watchDogQueue: newConnectionWatchDogQueue(),
+	}
+	go cb.watchDogQueue.Run(cb.Context())
+	//goland:noinspection GoVetCopyLock
+	return cb
+}
+
+// ContextWithTimeout should be used by Read and Write operations to obtain a context which expires
+// when both Read and Write have timed out.
+func (cb *ConnectionBase) ContextWithTimeout() context.Context {
+	return cb.watchDogQueue.ChangeGuard(cb.Context(), cb.Timeout())
 }
 
 // Context can be used to wait for cancellation of the Connection
 func (cb *ConnectionBase) Context() context.Context {
+	cb.mx.RLock()
+	defer cb.mx.RUnlock()
 	return cb.ctx
 }
 
 // ConnectionID is the ID of the connection.
 func (cb *ConnectionBase) ConnectionID() string {
+	cb.mx.RLock()
+	defer cb.mx.RUnlock()
 	return cb.connectionID
 }
 
 // SetConnectionID sets the ConnectionID
 func (cb *ConnectionBase) SetConnectionID(id string) {
+	cb.mx.Lock()
+	cb.mx.Unlock()
 	cb.connectionID = id
 }
 
 // Timeout is the timeout of the Connection
 func (cb *ConnectionBase) Timeout() time.Duration {
+	cb.mx.RLock()
+	defer cb.mx.RUnlock()
 	return cb.timeout
 }
 
 // SetTimeout sets the Timeout
 func (cb *ConnectionBase) SetTimeout(duration time.Duration) {
+	cb.mx.Lock()
+	cb.mx.Unlock()
 	cb.timeout = duration
 }
