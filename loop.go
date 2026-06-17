1b354a20e128b5e24333eddac0838cfc960b0c65
Rene Maget <37067579+rstdm@users.noreply.github.com>
2026-06-17 15:02:27 +0200

    Don't reset timeout when a ping is sent
    Sending a ping and receiving the corresponding TCP ACK doesn't prove that the websocket connection is still alive. For example, a misconfigured TCP proxy could acknowledge the transmission of the ping message even if the message hasn't reached its destination. The only way to prove that we're still able to receive messages from the other party is to receive an actual message.

--- loop.go
+++ loop.go
@@ -114,8 +114,6 @@ msgLoop:
 						break pingLoop
 					}
 				}
-				// A successful ping or Write shows us that the connection is alive. Reset the timeout
-				timeoutTicker.Reset(l.party.timeout())
 				// Don't break the pingLoop when keepAlive is over, it exists for this case
 			case <-timeoutTicker.C:
 				err = fmt.Errorf("timeout interval elapsed (%v)", l.party.timeout())
